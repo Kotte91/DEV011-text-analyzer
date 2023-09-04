08fc1cf804a989fd5ac3dbe7dffa3d2173dc23b8
Kotte91 <kottevaldebenito@gmail.com>
2023-09-04 13:12:52 -0300

    casi final

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,49 @@
-const analyzer = {  
-  getWordCount: (text) => {
+const analyzer = {
+  getWordCount: function (text) {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    if (!text) {
+      return 0;
+    }
+    const words = text.trim().split(/\s+/);
+    return words.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const textWithoutSpaces = text.replace(/\s+/g, '');
+    return textWithoutSpaces.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(/\s+/).filter(word => word !== '');
+    const totalLength = words.reduce((acc, word) => acc + word.length, 0);
+
+    if (words.length === 0) {
+      return "0.00"; // No hay palabras, retorna 0.00 como cadena
+    }
+
+    const averageLength = totalLength / words.length;
+    return averageLength.toFixed(2);
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g);
+    return numbers ? numbers.length : 0;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g);
+    if (numbers) {
+      let sum = 0;
+      for (let i = 0; i < numbers.length; i++) {
+        sum += parseFloat(numbers[i]);
+      }
+      return sum.toFixed(2);
+    }
+    return 0;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea = document.querySelector('textarea');
+const wordCountElement = document.querySelector('[data-testid="word-count"]');
+const characterCountElement = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCountElement = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCountElement = document.querySelector('[data-testid="number-count"]');
+const numberSumElement = document.querySelector('[data-testid="number-sum"]');
+const wordLengthAverageElement = document.querySelector('[data-testid="word-length-average"]');
+const resetButton = document.getElementById('reset-button');
+
+function updateResults() {
+  const text = textarea.value;
+
+  wordCountElement.textContent = `Recuento de palabras: ${analyzer.getWordCount(text)}`;
+  characterCountElement.textContent = `Recuento de caracteres: ${analyzer.getCharacterCount(text)}`;
+  characterNoSpacesCountElement.textContent = `Recuento de caracteres (sin espacios ni puntuación): ${analyzer.getCharacterCountExcludingSpaces(text)}`;
+  numberCountElement.textContent = `Recuento de números: ${analyzer.getNumberCount(text)}`;
+  numberSumElement.textContent = `Suma total de números: ${analyzer.getNumberSum(text)}`;
+  wordLengthAverageElement.textContent = `Longitud media de palabras: ${analyzer.getAverageWordLength(text)}`;
+}
+
+document.getElementsByName("user-input")[0].addEventListener('keyup', function () {
+  // Código para manejar el evento keyu en el elemento con name "user-input"p
+});
+textarea.addEventListener('input', updateResults);
+
+resetButton.addEventListener('click', () => {
+  textarea.value = ''; // Limpiar el textarea
+  updateResults(); // Actualizar resultados vacíos
+});
+
+// Actualizar resultados iniciales
+updateResults();
